31e6e527ec9742d28ff38a8147a2febed352ec38
Nicole Kelley <navkelley@gmail.com>
2017-03-05 13:01:11 -1000

    fix tests

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -5,6 +5,7 @@ import YouTubeVideoList from '../containers/youtube_video_list';
 import SearchBar from '../containers/search_bar';
 import TwitchVideoList from '../containers/twitch_video_list';
 import TwitchVideoDetail from './twitch/twitch_video_detail';
+import Logo from './img';
 
 
 export default class App extends Component {
@@ -19,9 +20,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>
-          <img src="../../../images/level-up.png" />
-        </h1>
+        <Logo src="../images/level-up.jpg" alt="level-up-logo" />
         <SearchBar />
         <YouTubeVideoDetail video={this.state.selectedVideo} />
         <YouTubeVideoList
---
+++ src/js/components/img.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logo = (props) => {
+  return (
+    <img src={props.src} alt={props.alt} />
+  );
+};
+
+export default Logo;

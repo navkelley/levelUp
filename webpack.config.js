656d459a24165d244e21aa1c3c1827852eca8968
github username <github email address>
2017-03-09 22:03:31 -1000

    align search bar center

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 require('babel-polyfill');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const path = require('path');
 
@@ -8,6 +7,8 @@ const webpack = require('webpack');
 const packageData = require('./package.json');
 
 const filename = [packageData.name, packageData.version, 'js'];
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	entry: ['babel-polyfill', './src/js/index.js'],
@@ -31,14 +32,32 @@ module.exports = {
 				test: /\.css$/, 
 				loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) 
 			},
+			exports.loadImages = function ({ include, exclude, options } = {}) {
+				return {
+					module: {
+						rules: [
+							{
+								test: /\.(png|jpg|svg)$/,
+								include,
+								exclude,
+
+								use: {
+									loader: 'url-loader',
+									options,
+								},
+							},
+						],
+					},
+				};
+			}
 		]
 	},
 	plugins: [
 		new webpack.DefinePlugin({
 			'process.env': {
 			NODE_ENV: JSON.stringify('production')
-		}
-	}),
-	new ExtractTextPlugin('app.css')
+			}
+		}),
+		new ExtractTextPlugin('app.css')
 	]
 };

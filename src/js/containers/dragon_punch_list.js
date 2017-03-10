d4dd7ba0c7fb85aa6603c53bdaa6ce1493c1e35b
github username <github email address>
2017-03-10 12:53:45 -1000

    add table for yt and twitch combined

--- src/js/containers/dragon_punch_list.js
+++ src/js/containers/dragon_punch_list.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import YouTubeVideoListItem from '../components/youtube/youtube_video_list_item';
 import TwitchVideoListItem from '../components/twitch/twitch_video_list_item';
 
-class TwitchVideoList extends Component {
+class DragonPunchList extends Component {
 	render() {
 		return (
-			<div id="twitch-streams">
+			<div id='dragon-search'>
 				<table>
 					<thead>
 						<tr>
+							<th>YouTube</th>
 							<th>Twitch</th>
 						</tr>
 					</thead>
 					<tbody>
-						{this.props.streams.map((stream) => {
+						{this.props.videos.map((video) => {
 							return (
-								<tr key={stream._id}>
-									<TwitchVideoListItem
+								<tr key={video.etag}>
+									<YouTubeVideoListItem 
+										onVideoSelect={this.props.onVideoSelect}
+										video={video}
+									/>
+									<TwitchVideoListItem 
 										onStreamSelect={this.props.onStreamSelect}
 										stream={stream}
 									/>
 								</tr>
-							);
+							)
 						})}
 					</tbody>
 				</table>
@@ -30,10 +36,3 @@ class TwitchVideoList extends Component {
 		);
 	}
 }
-
-function mapStateToProps({ streams }) {
-	//when have key:value that are ident can reduce to just one
-	return { streams };
-}
-
-export default connect(mapStateToProps)(TwitchVideoList);
